943c29b07cad0938e83318d84fac01d720108c42
Ryan Troxler <ryan@kicksite.net>
2021-10-06 11:43:28 -0500

    Add conditional organization

--- lib/interactor/organizer.rb
+++ lib/interactor/organizer.rb
@@ -21,6 +21,58 @@ module Interactor
       end
     end
 
+    class FilterableInteractorCollection
+      attr_reader :collection
+
+      def initialize
+        @collection = []
+      end
+
+      def run(interactor_class, filters = {})
+        interface = Interactor::Organizer::FilterableInteractor.new(interactor_class, filters)
+        collection << interface if interface.interactor_class
+      end
+
+      def add(interactor_classes)
+        interactor_classes.flatten.each { |interactor_class| run(interactor_class) }
+      end
+
+      def each(&block)
+        collection.each(&block) if block
+      end
+    end
+
+    class FilterableInteractor
+      attr_reader :filters, :interactor_class
+
+      CONDITIONAL_FILTERS = %i[if unless].freeze
+
+      def initialize(interactor_class, options = {})
+        @interactor_class = interactor_class
+        @filters = options.select { |key, _value| CONDITIONAL_FILTERS.include?(key) }
+      end
+
+      def call!(target, context)
+        return if check_conditionals(target, :if) == false
+        return if check_conditionals(target, :unless) == true
+
+        interactor_class.send(:call!, context)
+      end
+
+      private
+
+      def check_conditionals(target, filter)
+        resolve_option(target, filters[filter])
+      end
+
+      def resolve_option(target, opt)
+        return unless opt
+
+        return target.send(opt) if opt.is_a?(Symbol)
+        return target.instance_exec(&opt) if opt.is_a?(Proc)
+      end
+    end
+
     # Internal: Interactor::Organizer class methods.
     module ClassMethods
       # Public: Declare Interactors to be invoked as part of the
@@ -44,8 +96,10 @@ module Interactor
       #   end
       #
       # Returns nothing.
-      def organize(*interactors)
-        @organized = interactors.flatten
+      def organize(*interactors, &block)
+        organized.add(interactors) if interactors
+        organized.instance_eval(&block) if block
+        organized
       end
 
       # Internal: An Array of declared Interactors to be invoked.
@@ -63,7 +117,7 @@ module Interactor
       #
       # Returns an Array of Interactor classes or an empty Array.
       def organized
-        @organized ||= []
+        @organized ||= Interactor::Organizer::FilterableInteractorCollection.new
       end
     end
 
@@ -76,7 +130,7 @@ module Interactor
       # Returns nothing.
       def call
         self.class.organized.each do |interactor|
-          interactor.call!(context)
+          interactor.call!(self, context)
         end
       end
     end
--- spec/interactor/organizer_spec.rb
+++ spec/interactor/organizer_spec.rb
@@ -1,56 +1,107 @@
+require 'byebug'
+
 module Interactor
   describe Organizer do
     include_examples :lint
+    class Interactor2; end
+    class Interactor3; end
+    class Interactor4; end
 
     let(:organizer) { Class.new.send(:include, Organizer) }
 
     describe ".organize" do
-      let(:interactor2) { double(:interactor2) }
-      let(:interactor3) { double(:interactor3) }
+      let(:interactor2) { Interactor2 }
+      let(:interactor3) { Interactor3 }
 
       it "sets interactors given class arguments" do
-        expect {
-          organizer.organize(interactor2, interactor3)
-        }.to change {
-          organizer.organized
-        }.from([]).to([interactor2, interactor3])
+        organizer.organize(interactor2, interactor3)
+        expect(organizer.organized.collection.map(&:interactor_class))
+          .to eql([Interactor::Interactor2, Interactor::Interactor3])
       end
 
       it "sets interactors given an array of classes" do
-        expect {
-          organizer.organize([interactor2, interactor3])
-        }.to change {
-          organizer.organized
-        }.from([]).to([interactor2, interactor3])
+        organizer.organize([interactor2, interactor3])
+        expect(organizer.organized.collection.map(&:interactor_class))
+          .to eql([Interactor::Interactor2, Interactor::Interactor3])
+      end
+
+      it "accepts a block" do
+        organizer.organize do
+          run Interactor2
+          run Interactor3
+        end
+        expect(organizer.organized.collection.map(&:interactor_class))
+          .to eql([Interactor::Interactor2, Interactor::Interactor3])
       end
     end
 
     describe ".organized" do
       it "is empty by default" do
-        expect(organizer.organized).to eq([])
+        expect(organizer.organized.collection).to eq([])
       end
     end
 
     describe "#call" do
       let(:instance) { organizer.new }
       let(:context) { double(:context) }
-      let(:interactor2) { double(:interactor2) }
-      let(:interactor3) { double(:interactor3) }
-      let(:interactor4) { double(:interactor4) }
+      let(:interactor2) { Interactor2 }
+      let(:interactor3) { Interactor3 }
+      let(:interactor4) { Interactor4 }
 
-      before do
-        allow(instance).to receive(:context) { context }
-        allow(organizer).to receive(:organized) {
-          [interactor2, interactor3, interactor4]
-        }
+      context "without filtered interactors" do
+        before do
+          allow(instance).to receive(:context) { context }
+          allow(organizer).to receive(:organized) {
+            [interactor2, interactor3, interactor4]
+          }
+        end
+
+        it "calls each interactor in order with the context" do
+          expect(interactor2).to receive(:call!).once.with(instance, context).ordered
+          expect(interactor3).to receive(:call!).once.with(instance, context).ordered
+          expect(interactor4).to receive(:call!).once.with(instance, context).ordered
+
+          instance.call
+        end
       end
 
-      it "calls each interactor in order with the context" do
-        expect(interactor2).to receive(:call!).once.with(context).ordered
-        expect(interactor3).to receive(:call!).once.with(context).ordered
-        expect(interactor4).to receive(:call!).once.with(context).ordered
+      context "with interactors filtered by a proc" do
+        before do
+          allow(instance).to receive(:context) { context }
+          organizer.organize do
+            run Interactor2
+            run Interactor3, if: -> { false }
+            run Interactor4
+          end
+        end
 
-        instance.call
+        it "calls each interactor in order with the context" do
+          expect(Interactor2).to receive(:call!).once.with(context).ordered
+          expect(Interactor3).to_not receive(:call!)
+          expect(Interactor4).to receive(:call!).once.with(context).ordered
+
+          instance.call
+        end
+      end
+
+      context "with interactors filtered by a method symbol" do
+        before do
+          def instance.should_run_interactor_3?; false; end
+          allow(instance).to receive(:context) { context }
+          organizer.organize do
+            run Interactor2
+            run Interactor3, if: :should_run_interactor_3?
+            run Interactor4
+          end
+        end
+
+        it "calls each interactor in order with the context" do
+          expect(Interactor2).to receive(:call!).once.with(context).ordered
+          expect(Interactor3).to_not receive(:call!)
+          expect(Interactor4).to receive(:call!).once.with(context).ordered
+
+          instance.call
+        end
       end
     end
   end
